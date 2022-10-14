fe313c99f253ebad9bddd491780ee5db9b35f396
jathurchan <admin@jathurchan.com>
2022-10-14 10:21:00 +0200

    v2

--- web/tracker/models.py
+++ web/tracker/models.py
@@ -1,17 +1,50 @@
 from email.policy import default
-from turtle import title
+from pyexpat import model
+from unittest.util import _MAX_LENGTH
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-class Job(models.Model):
-    title = models.CharField(max_length=64)
-    description = models.TextField(blank=True)
+import uuid
 
-class Period(models.Model):
-    startDate = models.DateField()
-    endDate = models.DateField()
-    isOpen = models.BooleanField(default=True)
-    allotedTime = models.IntegerField()
 
 class User(AbstractUser):
-    job = models.ForeignKey("Job", blank=True, null=True, on_delete=models.CASCADE, related_name="users")
+    pass
+
+
+class Period(models.Model):
+    isActive = models.BooleanField(default=True)
+    startDate = models.DateField()
+    endDate = models.DateField()
+    allottedHours = models.IntegerField()
+
+
+class Project(models.Model):
+    key = models.CharField(max_length=4)    # PPPP
+    
+
+class Group(models.Model):
+    key = models.CharField(max_length=2)    # GG
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name="groups")
+
+
+class Task(models.Model):
+    key = models.CharField(max_length=4)    # TTTT
+    group = models.ForeignKey(Group, on_delete=models.CASCADE, related_name="tasks")
+    assignees = models.ManyToManyField(User, related_name="tasks")
+    description = models.TextField()
+
+
+class Sample(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False) # every sample is unique and identified by UUID v4
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="samples")
+    period = models.ForeignKey(Period, on_delete=models.CASCADE, related_name="samples")
+
+    task = models.ForeignKey(Task, on_delete=models.CASCADE, related_name="samples")
+
+    hours = models.IntegerField()
+    amount = models.DecimalField(default=0, max_digits=9, decimal_places=2)
+
+    updated = models.DateTimeField()
+    author = models.ForeignKey(User, on_delete=models.DO_NOTHING, related_name="edited_samples")
+    
--- web/tracker/views.py
+++ web/tracker/views.py
@@ -1,29 +1,28 @@
+import json
 from django.contrib.auth import authenticate, login, logout
-from django.db import IntegrityError
 from django.shortcuts import HttpResponse, HttpResponseRedirect, render
 from django.urls import reverse
+from django.http import JsonResponse
 from django.contrib.auth.decorators import login_required
 
-from .models import Job, User, Period
-
+from .models import User
 
 def index(request):
-    return HttpResponse("Hello, World!")
+    return HttpResponse("Hello, world!")
 
 
-def signin(request):
+def signin_view(request):
     if request.method == "POST":
+
         # Attempt to sign user in
         username = request.POST["username"]
         password = request.POST["password"]
-        print(username)
-        print(password)
         user = authenticate(request, username=username, password=password)
 
         # Check if authentication successful
         if user is not None:
             login(request, user)
-            return HttpResponseRedirect(reverse("add-sample"))
+            return HttpResponseRedirect(reverse("add"))
         else:
             return render(request, "tracker/signin.html", {
                 "message": "Invalid username or password."
@@ -31,63 +30,20 @@ def signin(request):
     else:
         return render(request, "tracker/signin.html")
 
-
-def signout(request):
+def signout_view(request):
     logout(request)
     return HttpResponseRedirect(reverse("signin"))
 
-
-def createUser(request):
-    if request.method == "POST":
-        username = request.POST["username"]
-
-        firstName = request.POST["firstName"]
-        lastName = request.POST["lastName"]
-
-        # Ensure password matches confirmation
-        password = request.POST["password"]
-        confirmation = request.POST["confirmation"]
-        if password != confirmation:
-            return render(request, "tracker/create-user.html", {
-                "message": "Passwords must match."
-            })
-
-        # Attempt to create new user
-        try:
-            user = User.objects.create_user(username=username, first_name=firstName, last_name=lastName, password=password)
-            user.job = Job.objects.get(id=request.POST["job_id"])
-            print(user.job)
-            user.save()
-        except IntegrityError:
-            return render(request, "tracker/create-user.html", {
-                "message": "Username already taken."
-            })
-        return HttpResponseRedirect(reverse("index"))
-    
-    all_jobs = list(Job.objects.all())
-
-    return render(request, "tracker/create-user.html", {
-        "jobs": all_jobs
-    })
-
 @login_required
-def addSample(request):
-    if request.method == "POST" :
-        try:
-            return addSampleForPeriod(request, request.POST["period_id"])
-        except:
-            pass
-        
-    open_periods = list(Period.objects.filter(isOpen=True))
-    return render(request, "tracker/add-sample.html", {
-        "open_periods": open_periods
-    })
+def add_view(request):
 
-@login_required
-def addSampleForPeriod(request, id):
+    # Get the signedin user
+    user = None
 
-    period = Period.objects.get(id=id)
+    if request.user.is_authenticated:
+        user = User.objects.get(username=request.user.username)
 
-    return render(request, "tracker/add-sample.html", {
-        "selected_period": period
+
+    return render(request, "tracker/add.html", {
+        "user": user
     })
